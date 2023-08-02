6d0fc447bd4c5c40cb222822a27c73fdf05a9c26
João Gabriel Manochio <joaogmns123@gmail.com>
2023-08-01 23:26:50 -0300

    feat(facil-consulta-api): create cidades table

---
+++ app/Models/Cidade.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cidade extends Model
+{
+    use HasFactory;
+
+    // /**
+    //  * @return \Illuminate\Database\Eloquent\Relations\HasMany
+    //  **/
+    // public function medicos()
+    // {
+    //     return $this->hasMany(Medico::class, 'cidade_id');
+    // }
+}
