e430c487ef2fe0b0f88a5e710c42e2516f0262c2
João Gabriel Manochio <joaogmns123@gmail.com>
2023-08-03 16:34:53 -0300

    feat(facil-consulta-api): adding test for store pacientes

--- tests/Feature/PacienteTest.php
+++ tests/Feature/PacienteTest.php
@@ -4,7 +4,9 @@ namespace Tests\Feature;
 
 use App\Models\Medico;
 use App\Models\MedicoPaciente;
+use App\Models\Paciente;
 use App\Models\User;
+use Faker\Generator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -58,6 +60,166 @@ class PacienteTest extends TestCase
         $response->assertStatus(200);
         $this->assertEmpty($response->getData());
         $this->assertIsArray($response->getData());
+    }
+
+    /**
+     * Should store a new patient
+     */
+    public function test_should_create_patient(): void
+    {
+        $faker = app(Generator::class);
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $faker->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $token = self::getToken();
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(201);
+        $this->assertNotEmpty($response->getData());
+        $this->assertEquals($input['cpf'], $response['cpf']);
+        $this->assertEquals($input['nome'], $response['nome']);
+    }
+
+    /**
+     * Should not create a new patient with missing fields
+     */
+    public function test_should_not_create_patient_with_missing_fields(): void
+    {
+        $faker = app(Generator::class);
+
+        // Missing nome
+        $input = [
+            'cpf' => $faker->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $token = self::getToken();
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(422);
+        $this->assertEquals('The nome field is required.', $response['message']);
+
+        // Missing cpf
+        $input = [
+            'nome' => $faker->name,
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(422);
+        $this->assertEquals('The cpf field is required.', $response['message']);
+
+        // Missing celular
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $faker->cpf,
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(422);
+        $this->assertEquals('The celular field is required.', $response['message']);
+    }
+
+    /**
+     * Should not create a new patient with existent cpf
+     */
+    public function test_should_not_create_patient_with_existent_cpf(): void
+    {
+        $faker = app(Generator::class);
+
+        $patient = Paciente::factory()->create();
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $patient->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $token = self::getToken();
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(422);
+        $this->assertEquals('CPF informado já existente!', $response['message']);
+    }
+
+    /**
+     * Should not create a new patient with invalid cpf
+     */
+    public function test_should_not_create_patient_with_invalid_cpf(): void
+    {
+        $faker = app(Generator::class);
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => '999.999.999-99',
+            'celular' => $faker->cellphone
+        ];
+
+        $token = self::getToken();
+
+        $headers = [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token
+        ];
+
+        $response = $this->json(
+            'POST',
+            route('pacientes.store'),
+            $input,
+            $headers
+        );
+
+        $response->assertStatus(422);
+        $this->assertEquals('O CPF informado não é válido', $response['message']);
     }
 
     /**
--- tests/Unit/PacienteTest.php
+++ tests/Unit/PacienteTest.php
@@ -4,6 +4,8 @@ namespace Tests\Unit;
 
 use App\Models\Medico;
 use App\Models\MedicoPaciente;
+use App\Models\Paciente;
+use Faker\Generator;
 use App\Services\Interfaces\PacienteServiceInterface;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -45,5 +47,110 @@ class PacienteTest extends TestCase
         $response = $serviceDoctor->listPatients($doctor->id);
 
         $this->assertEmpty($response);
+    }
+
+    /**
+     * Should create a new patient.
+     */
+    public function test_should_create_patient(): void
+    {
+        $service = app(PacienteServiceInterface::class);
+
+        $faker = app(Generator::class);
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $faker->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNotNull($response);
+        $this->assertEquals($input['nome'], $response->nome);
+        $this->assertEquals($input['cpf'], $response->cpf);
+        $this->assertInstanceOf(Paciente::class, $response);
+    }
+
+    /**
+     * Should not create a new patient with missing fields.
+     */
+    public function test_should_not_create_patient_with_missing_fields(): void
+    {
+        $service = app(PacienteServiceInterface::class);
+
+        $faker = app(Generator::class);
+
+        // missing nome
+        $input = [
+            'cpf' => $faker->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNull($response);
+
+        // missing cpf
+        $input = [
+            'nome' => $faker->name,
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNull($response);
+
+        // missing celular
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $faker->cpf,
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNull($response);
+    }
+
+    /**
+     * Should not create a new patient with with existent cpf.
+     */
+    public function test_should_not_create_patient_with_existent_cpf(): void
+    {
+        $service = app(PacienteServiceInterface::class);
+
+        $patient = Paciente::factory()->create();
+
+        $faker = app(Generator::class);
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => $patient->cpf,
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNull($response);
+    }
+
+    /**
+     * Should not create a new patient with with invalid cpf.
+     */
+    public function test_should_not_create_patient_with_invalid_cpf(): void
+    {
+        $service = app(PacienteServiceInterface::class);
+
+        $faker = app(Generator::class);
+
+        $input = [
+            'nome' => $faker->name,
+            'cpf' => '999.999.999-99',
+            'celular' => $faker->cellphone
+        ];
+
+        $response = $service->storePatient($input);
+
+        $this->assertNull($response);
     }
 }
